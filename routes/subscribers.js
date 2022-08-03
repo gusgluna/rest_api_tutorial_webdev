6620da465da1e4b4819eae193c3d9c1eb198cbb1
gusgluna <gusgluna@gmail.com>
2022-08-03 14:18:37 -0500

    first commit

---
+++ routes/subscribers.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+// Getting all
+router.get("/", (req, res) => {
+  res.send("hello world!!!");
+});
+
+// Getting one
+router.get("/:id", (req, res) => {
+  res.send("el id es " + req.params.id);
+});
+
+// Creating one
+router.post("/", (req, res) => {});
+
+// Updating one
+router.patch("/:id", (req, res) => {});
+
+// Deleting one
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
